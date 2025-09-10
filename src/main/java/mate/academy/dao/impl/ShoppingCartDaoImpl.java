8f0ffd446bb4564512294ee17bd827b3175ee682
Andrzej Pietkiewicz <andrzej.pietkiewicz@orange.com>
2025-09-10 13:36:42 +0200

    initial commit

---
+++ src/main/java/mate/academy/dao/impl/ShoppingCartDaoImpl.java
@@ -0,0 +1,65 @@
+package mate.academy.dao.impl;
+
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import mate.academy.dao.ShoppingCartDao;
+import mate.academy.exception.DataProcessingException;
+import mate.academy.lib.Dao;
+import mate.academy.model.Movie;
+import mate.academy.model.MovieSession;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.User;
+import mate.academy.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import java.util.Optional;
+
+@Dao
+public class ShoppingCartDaoImpl implements ShoppingCartDao {
+    @Override
+    public ShoppingCart add(ShoppingCart shoppingCart) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.persist(shoppingCart);
+            transaction.commit();
+            return shoppingCart;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert shopping cart " + shoppingCart, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public Optional<ShoppingCart> getByUser(User user) {
+       try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+           Query<ShoppingCart> query = session.createQuery(
+                   "FROM ShoppingCart sc "
+                   + "LEFT JOIN FETCH sc.tickets "
+                   + "WHERE sc.user = :user", ShoppingCart.class
+           );
+           query.setParameter("user", user);
+           return Optional.ofNullable(query.uniqueResult());
+       } catch (Exception e) {
+           throw new DataProcessingException("Can't get shopping cart for user: " + user, e);
+       }
+    }
+
+    @Override
+    public void update(ShoppingCart shoppingCart) {
+
+    }
+}
---
+++ src/main/java/mate/academy/service/ShoppingCartService.java
@@ -0,0 +1,23 @@
+package mate.academy.service;
+
+import mate.academy.dao.TicketDao;
+import mate.academy.model.MovieSession;
+import mate.academy.model.ShoppingCart;
+import mate.academy.model.Ticket;
+import mate.academy.model.User;
+
+public interface ShoppingCartService extends TicketDao {
+    /**
+     * This method is responsible for adding a Ticket to the ShoppingCart
+     * @param movieSession contains the information required for the ticket
+     * @param user - the User who wants to buy the ticket for a specific movieSession
+     */
+    void addSession(MovieSession movieSession, User user);
+
+    ShoppingCart getByUser(User user);
+
+    void registerNewShoppingCart(User user);
+
+    void clear(ShoppingCart shoppingCart);
+
+}
